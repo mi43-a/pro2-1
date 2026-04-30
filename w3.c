1d6ef8564470868610612e9113a349c0a2143980
mi43-a <195055665+mi43-a@users.noreply.github.com>
2026-04-30 21:41:53 +0900

    c3-1

---
+++ w3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int apple = 100, orange = 88, strawberry = 398;
+    int a_count, o_count, s_count;
+    int total_price;
+
+    printf("Enter the quantity of each fruit:\n");
+    printf("Apple: ");
+    scanf("%d", &a_count);
+    printf("Orange: ");
+    scanf("%d", &o_count);
+    printf("Strawberry: ");
+    scanf("%d", &s_count);
+
+    total_price = (a_count * apple) + (o_count * orange) + (s_count * strawberry);
+
+    printf("Total price is %d yen", total_price);
+    
+    return 0;
+}
