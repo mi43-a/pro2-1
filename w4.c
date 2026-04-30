ccb48bc7bed60a384a8d4ca1b5cb95d5bf4ae230
mi43-a <195055665+mi43-a@users.noreply.github.com>
2026-04-30 22:13:27 +0900

    c4-5

--- w4.c
+++ w4.c
@@ -4,16 +4,25 @@ int main()
     int a, b, c;
     int total_players, remaining_players;
 
-    printf("Enter the number of students in the class: ");
-    scanf("%d", &a);
-    printf("Enter the number of baseball teams: ");
-    scanf("%d", &b);
-    printf("Enter the number of volleyball teams: ");
-    scanf("%d", &c);
-    total_players = (9 * b) + (6 * c);
+    while(1)
+    {
+        printf("Enter the number of students in the class: ");
+        scanf("%d", &a);
+        printf("Enter the number of baseball teams: ");
+        scanf("%d", &b);
+        printf("Enter the number of volleyball teams: ");
+        scanf("%d", &c);
+        total_players = (9 * b) + (6 * c);
+
+        if (a >= total_players) {
+            break; 
+        } else {
+            printf("Error: Not enough students! Please try again.\n");
+        }
+    }
     remaining_players = a - total_players;
     printf("Total players: %d\n", total_players);
-    printf("Remaining players after one is removed: %d\n", remaining_players);
+    printf("Students who cannot participate: %d\n", remaining_players);
     
     return 0;
 }
